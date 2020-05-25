8aa894bdae39bb5b8a0fee6951a596f230987821
Jihun Park <jxxparker@gmail.com>
2020-05-25 17:28:06 -0500

    while

---
+++ src/WhileLoop/WhileLoop.java
@@ -0,0 +1,25 @@
+package WhileLoop;
+
+public class WhileLoop {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int count = 1;
+		while(count != 6) {
+			System.out.println("count value is " + count);
+			count++;
+		}
+		System.out.println("******************");
+		count = 1;
+		while(true) {
+			if(count == 6 ) {
+				break;
+			}
+			System.out.println("count value is " + count);
+			count++;
+
+		}
+		
+	}
+
+}
