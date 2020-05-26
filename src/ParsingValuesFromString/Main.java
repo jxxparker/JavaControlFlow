78897749edee633ad5b86ab12bf5cdd1cc26f338
Jihun Park <jxxparker@gmail.com>
2020-05-26 16:25:16 -0500

    string to int

--- src/ParsingValuesFromString/Main.java
+++ src/ParsingValuesFromString/Main.java
@@ -4,7 +4,19 @@ public class Main {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		String numberAsString = "2020.3";
+		System.out.println(numberAsString);
+		
+		double number = Double.parseDouble(numberAsString);
+		System.out.println("hey " + number);
+		
+		numberAsString += 1;
+		number += 1;
+		
+		System.out.println("new number " + numberAsString);
+		System.out.println("real new number "+ number);
+		
+		
 	}
 
 }
