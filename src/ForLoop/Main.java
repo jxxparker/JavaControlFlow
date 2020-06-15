76b65c121eaa31088444843f5e27e50fa9705c4d
Jihun Park <jxxparker@gmail.com>
2020-06-15 13:35:43 -0500

    trying forloops

---
+++ src/ForLoop/Main.java
@@ -0,0 +1,24 @@
+package ForLoop;
+
+public class Main {
+
+	public static void main(String[] args) {
+		
+		for(int i = 0; i < 1; i++) { //printing it 9x
+			System.out.println("i love dogs");
+		}
+		
+		for(int i = 0; i < 1; i++) {
+			System.out.println(i);
+		}
+		
+		int[] grades = {90,100,80,93};
+		
+		for(int i = 0; i <grades.length; i ++) {
+			System.out.println(i + " grades are " + grades[i]);
+		}
+		
+		
+	}
+
+}
---
+++ src/WhileLoop/Whileloop2.java
@@ -0,0 +1,13 @@
+package WhileLoop;
+
+public class Whileloop2 {
+	public static void main(String[] args) {
+		
+		int a = 0;
+		
+	do {
+		System.out.println("a");
+	} while (a == 1);
+	
+	}
+}
