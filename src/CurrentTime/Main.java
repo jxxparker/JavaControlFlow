6f5c573fecc5c2c9713d84492dabd436be42c259
Jihun Park <jxxparker@gmail.com>
2020-06-15 14:36:05 -0500

    more stuff

---
+++ src/CurrentTime/Main.java
@@ -0,0 +1,13 @@
+package CurrentTime;
+
+import java.util.Date;
+
+public class Main {
+
+	public static void main(String[] args) {
+		Date currentDate = new Date();
+		System.out.println(currentDate);
+
+	}
+
+}
